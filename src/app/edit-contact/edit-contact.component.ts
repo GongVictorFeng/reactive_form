0235c64ea270057751768fe6af1394d0aea19db3
victor <gfeng005@uottawa.ca>
2025-04-12 21:03:18 -0400

    added reactive form to the edit contact component.

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
+import { FormBuilder } from '@angular/forms';
+import { AddressTypes, PhoneTypes } from '../contacts/contact.model';
 
 @Component({
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  phoneTypes = PhoneTypes;
+  addressTypes = AddressTypes;
+  contactForm = this.fb.nonNullable.group({
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
+    personal: false,
+    phone: this.fb.nonNullable.group({
+      phoneNumber: '',
+      phoneType: '',
+    }),
+    address: this.fb.nonNullable.group({
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    }),
+    notes: '',
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private contactService: ContactsService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
+    this.contactService.getContact(contactId).subscribe({
+      next: (contact) => {
+        if (contact) {
+          this.contactForm.setValue(contact);
+        }
+      },
+    });
   }
 
   saveContact() {
-
+    console.log(this.contactForm.value);
+    this.contactService.saveContact(this.contactForm.getRawValue()).subscribe({
+      next: () => this.router.navigate(['/contacts']),
+    });
   }
 }
