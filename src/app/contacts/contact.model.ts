35d25d0fddd7794e0c2667bd3797f301a6146aa0
victor <gfeng005@uottawa.ca>
2025-04-12 21:01:54 -0400

    completed the contact model.

--- src/app/contacts/contact.model.ts
+++ src/app/contacts/contact.model.ts
@@ -1,22 +1,36 @@
 export interface Contact {
-  id: string,
-  firstName: string,
-  lastName: string,
-  dateOfBirth: Date | null,
-  favoritesRanking: number | null,
-  phone: Phone,
-  address: Address,
+  id: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date | null;
+  favoritesRanking: number | null;
+  personal: boolean;
+  phone: Phone;
+  address: Address;
+  notes: string;
 }
 
 export interface Phone {
-  phoneNumber: string,
-  phoneType: string,
+  phoneNumber: string;
+  phoneType: string;
 }
 
 export interface Address {
-  streetAddress: string,
-  city: string,
-  state: string,
-  postalCode: string,
-  addressType: string,
+  streetAddress: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  addressType: string;
 }
+
+export const PhoneTypes = [
+  { title: 'Mobile', value: 'mobile' },
+  { title: 'Work', value: 'work' },
+  { title: 'Other', value: 'other' },
+];
+
+export const AddressTypes = [
+  { title: 'Home', value: 'home' },
+  { title: 'Work', value: 'work' },
+  { title: 'Other', value: 'other' },
+];
