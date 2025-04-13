7c9315a00b0a1e8ff294903d5913ec96031b85e8
victor <gfeng005@uottawa.ca>
2025-04-12 21:02:51 -0400

    modified the in memory server.

--- src/app/contacts/in-memory-contacts.service.ts
+++ src/app/contacts/in-memory-contacts.service.ts
@@ -10,14 +10,16 @@ export class InMemoryContactsApi implements InMemoryDbService {
         lastName: 'Doodleplumb',
         dateOfBirth: new Date('1994/05/05'),
         favoritesRanking: 0,
+        personal: false,
         phone: { phoneNumber: '555-765-4321', phoneType: 'mobile' },
         address: {
           streetAddress: '777 Whimsy Lane',
           city: 'Gleeberg City',
           state: 'Colohoma',
           postalCode: 'A4321',
-          addressType: 'home'
-        }
+          addressType: 'home',
+        },
+        notes: '',
       },
       {
         id: 'A6rwe',
@@ -25,14 +27,16 @@ export class InMemoryContactsApi implements InMemoryDbService {
         lastName: 'Flungford',
         dateOfBirth: new Date('1988/10/05'),
         favoritesRanking: 0,
+        personal: false,
         phone: { phoneNumber: '555-877-5678', phoneType: 'mobile' },
         address: {
           streetAddress: '543 Lullaby Lane',
           city: 'Sleepytown',
           state: 'Ulaska',
           postalCode: 'F2231',
-          addressType: 'other'
+          addressType: 'other',
         },
+        notes: '',
       },
       {
         id: '3bNGA',
@@ -40,17 +44,19 @@ export class InMemoryContactsApi implements InMemoryDbService {
         lastName: 'Giggleworth',
         dateOfBirth: new Date('1986/11/08'),
         favoritesRanking: 1,
+        personal: false,
         phone: { phoneNumber: '555-123-4567', phoneType: 'mobile' },
         address: {
           streetAddress: '123 Merriment Avenue',
           city: 'Dorado City',
           state: 'Mezona',
           postalCode: 'Z2345',
-          addressType: 'work'
+          addressType: 'work',
         },
+        notes: '',
       },
-    ]
+    ];
 
-    return { contacts }
+    return { contacts };
   }
 }
